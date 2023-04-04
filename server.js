bdd04fddf9981aaf665f249821b016df281a4b13
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-04-04 20:44:56 +0530

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
